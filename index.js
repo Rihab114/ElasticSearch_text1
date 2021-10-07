5f86f51270a0d5012b4a351128400efca7531531
Rihab <75638904+Rihab114@users.noreply.github.com>
2021-10-07 12:05:17 +0200

    Update index.js

--- index.js
+++ index.js
@@ -7,68 +7,52 @@ var bodyParser = require('body-parser');
 var app     = express();
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: true}));
-  const port = 8080
+const port = 8080
 
-  app.use(function(req, res, next) {
+app.use(function(req, res, next) {
     res.header("Access-Control-Allow-Origin", "*");
     res.header('Access-Control-Allow-Methods', 'PUT, GET, POST, DELETE, OPTIONS');
     res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
     next();
-  });
+});
 
-app.locals.data = "";
-
-
-
-app.post('/search', function(req,next){
-  var item = req.body.tagToSearch;
-  req.app.locals.data = item;
-
-
-
-})
-  // charger la page html
-  app.get('/',function(req,res) {
-    res.sendFile(__dirname +'/search.html');
-  });
+app.locals.data=""
 
 // defenir le contenu de route /search
-app.get('/search', function (req, res){
+app.get('/', function (req, res){
+  res.sendFile(__dirname +'/search.html');
+  
+  app.post('/', function(req,res){
+  var item = req.body.tagToSearch;
   client.search({
 
     index : 'flickrphotos3',
     body: {"size" : 4000 ,
 
 
-    "query": {"fuzzy": {"tags":  req.app.locals.data }}}
+    "query": {"fuzzy": {"tags": item}}}
  
 
     
 }) .then(req => {
-  res.send(req.body.hits.hits);
-  console.log(req.body.hits.hits);
+  hits = req.body.hits.hits;
+  console.log(hits)
+ 
+ 
 })
-    
+  console.log(item)
+  })
+  
+
 
 }, function (err) {
 console.trace(err.message);
 });
 
 
-//app.post('/search', function(req,next){
-  //var item = req.body.tagToSearch;
-  //console.log(item)
-//});
-
-
-  
-
-
-
   app.listen(port, () => {
     console.log(`Example app listening at http://localhost:${port}`)
   })
-
 
 
   module.exports = app;
